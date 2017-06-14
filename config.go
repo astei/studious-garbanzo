7583a709682eff91d9d902916dc49bc9a69e9c28
Tux <git@steinborn.me>
2017-06-14 05:54:59 -0400

    initial commit

---
+++ config.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"text/template"
+
+	"github.com/google/go-github/github"
+	"github.com/taskcluster/shell"
+)
+
+// Configuration is a Go representation of the contents of config.json.
+type Configuration struct {
+	// Where studious-garbanzo should listen.
+	Listen string `json:"Listen"`
+
+	// Security-related settings.
+	Security struct {
+		// Place the webhook at a custom subpath.
+		CustomPath string `json:"CustomPath"`
+
+		// A HMAC secret.
+		Secret string `json:"Secret"`
+	} `json:"Security"`
+
+	// The repositories to respond to. If a repository is not listed,
+	// studious-garbanzo will not do anything.
+	Repositories []struct {
+		// The repository to use.
+		Repository string `json:"Repository"`
+
+		// The commands to execute for this repository.
+		Commands []RepoCommand `json:"Commands"`
+	} `json:"Repositories"`
+}
+
+type RepoCommand struct {
+	// The command to execute.
+	Command string `json:"Command"`
+
+	// The arguments to use.
+	Args []string `json:"Args"`
+}
+
+// GetArgs gets the parsed and escaped arguments for the command.
+func (rc RepoCommand) GetArgs(e github.PushEvent, repo string) []string {
+	args := make([]string, len(rc.Args))
+	for i, arg := range rc.Args {
+		finalArg := arg
+		tmpl, err := template.New("").Parse(arg)
+		if err == nil {
+			var buf bytes.Buffer
+			if err = tmpl.Execute(&buf, e); err == nil {
+				finalArg = buf.String()
+			}
+		}
+		if err != nil {
+			log.Printf("NOTICE: arg '%s' (#%d) for '%s' (%s) is not valid: %s", arg, i, rc.Command, repo, err.Error())
+		}
+		args[i] = shell.Escape(finalArg)
+	}
+	return args
+}
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+
+	"io"
+
+	"strings"
+
+	"github.com/google/go-github/github"
+)
+
+var config Configuration
+
+const (
+	maxPayloadSize = 5 * 1024 * 1024 // 5MB
+)
+
+// onGitHubPush is the main HTTP handler function.
+func onGitHubPush(w http.ResponseWriter, r *http.Request) {
+	// smoke test
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var body bytes.Buffer
+	if _, err := body.ReadFrom(r.Body); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if config.Security.Secret != "" {
+		// Verify the secret
+		secret := r.Header.Get("X-Hub-Signature")
+		if secret == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "no signature")
+			return
+		}
+
+		mac := hmac.New(sha1.New, []byte(config.Security.Secret))
+		mac.Write(body.Bytes())
+		computed := "sha1=" + hex.EncodeToString(mac.Sum(nil))
+
+		if !hmac.Equal([]byte(computed), []byte(secret)) {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "invalid signature")
+			return
+		}
+	}
+
+	// check for ping (or any other events)
+	if r.Header.Get("X-GitHub-Event") != "push" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	var e github.PushEvent
+	var jsonBody io.Reader
+	if r.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+		v, err := url.ParseQuery(body.String())
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "invalid form")
+			return
+		}
+		jsonBody = strings.NewReader(v.Get("payload"))
+	} else {
+		jsonBody = &body
+	}
+	if err := json.NewDecoder(jsonBody).Decode(&e); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "invalid JSON")
+		return
+	}
+
+	for _, repoConfig := range config.Repositories {
+		if repoConfig.Repository == *e.Repo.FullName {
+			// Bake and execute!
+			for _, cmdConfig := range repoConfig.Commands {
+				// Now execute the commands
+				ex := exec.Command(cmdConfig.Command, cmdConfig.GetArgs(e, repoConfig.Repository)...)
+				if err := ex.Run(); err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					io.WriteString(w, "couldn't run command: "+err.Error())
+					return
+				}
+			}
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+
+	// oh well
+	w.WriteHeader(http.StatusBadRequest)
+	io.WriteString(w, "no matching repository found")
+}
+
+func main() {
+	cf, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalf("unable to open config.json: %s", err)
+	}
+
+	if err = json.NewDecoder(cf).Decode(&config); err != nil {
+		cf.Close()
+		log.Fatalf("unable to read config.json: %s", err)
+	}
+
+	cf.Close()
+	http.HandleFunc("/"+config.Security.CustomPath, onGitHubPush)
+	http.ListenAndServe(config.Listen, nil)
+}
