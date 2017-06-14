cb3ba9fc59943e8127dcd62736fa16a78167246b
Tux <git@steinborn.me>
2017-06-14 09:12:52 -0400

    fix script

--- example/remote-pull/remote-pull.sh
+++ example/remote-pull/remote-pull.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-if [ $1 -eq "refs/refs/heads/master"]; do
+if [ "$#" -ne 1 ]; then
+    echo "Don't run this shell script directly!"
+    exit 1
+fi
+
+if [ $1 == "refs/heads/master" ]; then
     git pull origin
+else
+    exit 1
 fi
